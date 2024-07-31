00a48fd59edec6b1f9087149a67ea2a7cdaf38c0
pebblewebble <ericlye123344@gmail.com>
2024-07-31 17:24:10 +0800

    Oops the return condition was wrong

--- duplicateInteger.cpp
+++ duplicateInteger.cpp
@@ -5,12 +5,12 @@ public:
   bool hasDuplicate(std::vector<int> &nums) {
     std::unordered_map<int, int> hashmap;
     for (int i = 0; i < nums.size(); i++) {
-      if (hashmap.find(nums[i]) != hashmap.end()) {
+      if (hashmap.find(nums[i]) == hashmap.end()) {
         hashmap.insert({nums[i], 1});
       } else {
-        return false;
+        return true;
       }
     }
-    return true;
+    return false;
   }
 };
