ad4d63ef5b60bc417f6aa800c0dbf6a31c434eee
pebblewebble <ericlye123344@gmail.com>
2024-07-31 20:53:10 +0800

    ensured that it does the smallest index first

--- twoIntSum.cpp
+++ twoIntSum.cpp
@@ -6,12 +6,16 @@ public:
   std::vector<int> twoSum(std::vector<int> &nums, int target) {
     std::unordered_map<int, int> hashmap;
     for (int i = 0; i < nums.size(); i++) {
-      if (hashmap.find(nums[i]) == hashmap.end()) {
-        hashmap.insert({nums[i], i});
-      }
       int difference=abs(target-nums[i]);
       if(hashmap.find(difference)!=hashmap.end()){
-        return {i,hashmap.at(difference)};
+        if(i<hashmap.at(difference)){
+          return {i,hashmap.at(difference)};
+        }else{
+          return {hashmap.at(difference),i};
+        }
+      }
+      if (hashmap.find(nums[i]) == hashmap.end()) {
+        hashmap.insert({nums[i], i});
       }
     }
     return {0,0};
