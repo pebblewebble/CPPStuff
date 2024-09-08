98ecbd1ea025999b17151bf35e3416e03aae61a4
pebblewebble <ericlye123344@gmail.com>
2024-09-08 23:41:56 +0800

    new function

--- AssignmentThings/code.cpp
+++ AssignmentThings/code.cpp
@@ -3,6 +3,6 @@ using namespace eric;
 using namespace std;
 int main() {
   files reviews=files("tripadvisor_hotel_reviews.csv"); 
-  reviews.recordsToArray();
+  reviews.recordsToArray(true);
   cout<<"End of program"<<endl;
 }
--- AssignmentThings/ericHeader.h
+++ AssignmentThings/ericHeader.h
@@ -7,72 +7,79 @@
 using namespace std;
 
 namespace eric {
-// Node class  
-class Node {  
-public:  
-  std::string data;  
-  Node *next;  
-  
-  Node(const std::string& data) {  
-    this->data = data;  
-    this->next = nullptr;  
-  }  
-};  
-  
-// Linked list class  
-class LinkedList {  
-private:  
-  Node *head;  
-  
-public:  
-  LinkedList() {  
-    this->head = nullptr;  
-  }  
-  
-  void insertAtBeginning(const std::string& data) {  
-    Node *newNode = new Node(data);  
-    newNode->next = head;  
-    head = newNode;  
-  }  
-  
-  void insertAtEnd( const std::string& data) {  
-    Node *newNode = new Node(data);  
-    if (head == nullptr) {  
-      head = newNode;  
-      return;  
-    }  
-    Node *temp = head;  
-    while (temp->next != nullptr) {  
-      temp = temp->next;  
-    }  
-    temp->next = newNode;  
-  }  
-  
-  void deleteAtBeginning() {  
-    if (head == nullptr) {  
-      return;  
-    }  
-    Node *temp = head;  
-    head = head->next;  
-    delete temp;  
-  }  
-  
-  void deleteAtEnd() {  
-  if (head == nullptr) {  
-    return;  
-  }  
-  if (head->next == nullptr) {  
-    delete head;  
-    head = nullptr;  
-    return;  
-  }  
-  Node *temp = head;  
-  while (temp->next->next != nullptr) {  
-    temp = temp->next;  
-  }  
-  delete temp->next;  
-  temp->next = nullptr;  
-}
+
+// Node class
+class Node {
+public:
+  std::string data;
+  Node *next;
+
+  Node(const std::string &data) {
+    this->data = data;
+    this->next = nullptr;
+  }
+};
+
+// Linked list class
+class LinkedList {
+private:
+  Node *head;
+
+public:
+  LinkedList() { this->head = nullptr; }
+
+  void insertAtBeginning(const std::string &data) {
+    Node *newNode = new Node(data);
+    newNode->next = head;
+    head = newNode;
+  }
+
+  void insertAtEnd(const std::string &data) {
+    Node *newNode = new Node(data);
+    if (head == nullptr) {
+      head = newNode;
+      return;
+    }
+    Node *temp = head;
+    while (temp->next != nullptr) {
+      temp = temp->next;
+    }
+    temp->next = newNode;
+  }
+
+  void deleteAtBeginning() {
+    if (head == nullptr) {
+      return;
+    }
+    Node *temp = head;
+    head = head->next;
+    delete temp;
+  }
+
+  void deleteAtEnd() {
+    if (head == nullptr) {
+      return;
+    }
+    if (head->next == nullptr) {
+      delete head;
+      head = nullptr;
+      return;
+    }
+    Node *temp = head;
+    while (temp->next->next != nullptr) {
+      temp = temp->next;
+    }
+    delete temp->next;
+    temp->next = nullptr;
+  }
+
+  ~LinkedList() {
+    while (head != nullptr) {
+      Node *temp = head;
+      head = head->next;
+      delete temp;
+    }
+  }
 };
 
 class files {
@@ -91,36 +98,51 @@ public:
   ~files() {
     if (fileOutput.is_open()) {
       fileOutput.close();
+      std::cout<< "File is successfully closed"<<endl;
     }
   }
   // std::string* recordsToArray(){
-  void recordsToArray() {
+  void recordsToArray(bool skipHeader) {
     std::string line;
     while (std::getline(fileOutput, line)) {
-      // The substring numbers used is to remove the unnecessary commas and
-      // stuff
-      line = line.substr(1, line.length() - 6);
-      lineSplit(line, ", ");
-      std::cout << line << std::endl;
-      std::cin >> line;
+      // Some files have a header that you might want to skip the first line
+      if (!skipHeader) {
+        // The substring numbers used is to remove the unnecessary commas and
+        // stuff
+        line = line.substr(1, line.length() - 6);
+        lineSplit(line, ", ");
+        std::cout << line << std::endl;
+        std::cin >> line;
+      }
+      skipHeader = false;
     }
   }
 
-  //Array seems to hard to implement for lineSplit cause we do not know the initial size, and recreating array everytime and
-  //copying the elements to new array is probably bad, so linked list might be best
+  // Array seems to hard to implement for lineSplit cause we do not know the
+  // initial size, and recreating array everytime and copying the elements to
+  // new array is probably bad, so linked list might be best
+
+  string removeSpecialCharacter(std::string& s){
+    for(int i=0;i<s.size();i++){
+      if(s[i]<'A'||s[i]>'Z' && s[i],'a'||s[i]>'z'){
+        s.erase(i,1);
+        i--;
+      }
+    }
+    return s;
+  }
 
   // std::string* lineSplit(std::string line, std::string delimiter) {
   void lineSplit(std::string line, std::string delimiter) {
     std::stringstream data(line);
     eric::LinkedList list;
     std::string word;
-    while (data>>word) {
+    while (data >> word) {
+
       list.insertAtEnd(word);
-      std::cout<<word<<std::endl;
+      std::cout << word << std::endl;
     }
   }
 };
-
-
 
 } // namespace eric
