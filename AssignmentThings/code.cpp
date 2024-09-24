8c4a447cd1137226217650636bf86288a2ec4d4b
pebblewebble <142425284+pebblewebble@users.noreply.github.com>
2024-09-25 00:19:12 +0800

    adding cli now

--- AssignmentThings/code.cpp
+++ AssignmentThings/code.cpp
@@ -1,78 +1,111 @@
 #include "ericHeader.h"
 #include <array>
+#include <chrono>
 #include <fstream>
 #include <iostream>
 #include <sstream>
 #include <string>
-#include <chrono>
 
 using namespace eric;
 using namespace std;
 using namespace chrono;
 int main() {
   auto start = high_resolution_clock::now();
-  eric::files reviews=eric::files("tripadvisor_hotel_reviews.csv"); 
-  // eric::files reviews=eric::files("test.csv"); 
-  //Read words and sort them based on alphabetic
-  //store them according to the first alphabet using hashmap 
+  eric::files reviews = eric::files("tripadvisor_hotel_reviews.csv");
+  // eric::files reviews=eric::files("test.csv");
+  // Read words and sort them based on alphabetic
+  // store them according to the first alphabet using hashmap
   // key : alphabet
   // value : dynamic array or linked list?
-  std::fstream& reviewsFileStream=reviews.getFileStream();
+  std::fstream &reviewsFileStream = reviews.getFileStream();
+
   eric::files positiveWordsFile = eric::files("positive-words.txt");
-  std::fstream& positiveWordsFileStream = positiveWordsFile.getFileStream();
-  eric::LinkedListSentimentWords  positiveWords = eric::LinkedListSentimentWords();
+  std::fstream &positiveWordsFileStream = positiveWordsFile.getFileStream();
+  eric::LinkedListSentimentWords positiveWords =
+      eric::LinkedListSentimentWords();
+
   eric::files negativeWordsFile = eric::files("negative-words.txt");
-  std::fstream& negativeWordsFileStream = negativeWordsFile.getFileStream();
-  eric::LinkedListSentimentWords  negativeWords = eric::LinkedListSentimentWords();
+  std::fstream &negativeWordsFileStream = negativeWordsFile.getFileStream();
+  eric::LinkedListSentimentWords negativeWords =
+      eric::LinkedListSentimentWords();
 
-
-  //Probably iterate through positive-words.txt and store them into positiveWords as a 2d array
-  //I'll do a linked list implementation first
+  // Probably iterate through positive-words.txt and store them into
+  // positiveWords as a 2d array I'll do a linked list implementation first
 
   std::string word;
-  while(std::getline(positiveWordsFileStream,word)){
+  while (std::getline(positiveWordsFileStream, word)) {
     positiveWords.addWord(word);
   }
 
-  while(std::getline(negativeWordsFileStream,word)){
+  while (std::getline(negativeWordsFileStream, word)) {
     negativeWords.addWord(word);
   }
+
+  // User CLI?
+  std::cout << "╔═════════════════════════════════════════════════════════════════╗\n";
+  std::cout << "║ ██╗    ██╗███████╗██╗      ██████╗ ██████╗ ███╗   ███╗███████╗  ║\n";
+  std::cout << "║ ██║    ██║██╔════╝██║     ██╔════╝██╔═══██╗████╗ ████║██╔════╝  ║\n";
+  std::cout << "║ ██║ █╗ ██║█████╗  ██║     ██║     ██║   ██║██╔████╔██║█████╗    ║\n";
+  std::cout << "║ ██║███╗██║██╔══╝  ██║     ██║     ██║   ██║██║╚██╔╝██║██╔══╝    ║\n";
+  std::cout << "║ ╚███╔███╔╝███████╗███████╗╚██████╗╚██████╔╝██║ ╚═╝ ██║███████╗  ║\n";
+  std::cout << "║  ╚══╝╚══╝ ╚══════╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝  ║\n";
+  std::cout << "╚═════════════════════════════════════════════════════════════════╝\n";
+  std::cout << "1. Choose a line to compare\n";
+  std::cout << "2. Check Linked List Speed\n";
+  std::cout << "3. Check Linked List Speed with Display Output\n";
+  std::cout << "   *Note: It compares every line\n";
+  // std::cout << "4. "
+
+  int option;
+  std::cin>>option;
+  int chosenLine=-1;
+
+  if(option==1){
+    std::cout << "Please enter the line number:\n";
+    std::cin >> chosenLine; 
+  }
+
 
   bool skipHeader = true;
   // bool skipHeader = false;
   int lineCount = 0;
   std::string line;
-  while (std::getline(reviewsFileStream, line)) {
-      // Some files have a header that you might want to skip the first line
-      if (!skipHeader) {
-        // The substring numbers used is to remove the unnecessary commas and
-        // stuff
-        int csvSentimentScore = std::stoi(line.substr(line.length()-1,line.length()));
-        line = line.substr(1, line.length() - 6);
-        eric::LinkedList list = eric::lineSplit(line, ", ");
-        // std::cout << line << std::endl;
-        // printList(list.head);
-        eric::mergeSort(&list.head);
-        // insertionSort(&list.head);
-        // printList(list.head);
 
-        // cin>>line;
-        double score=eric::findMatchingWord(list.head,positiveWords.data.head,negativeWords.data.head);
+  while (std::getline(reviewsFileStream, line)) {
+    // Some files have a header that you might want to skip the first line
+    if (!skipHeader) {
+      // The substring numbers used is to remove the unnecessary commas and
+      // stuff
+      int csvSentimentScore = std::stoi(line.substr(line.length() - 1, line.length()));
+      line = line.substr(1, line.length() - 6);
+      eric::LinkedList list = eric::lineSplit(line, ", ");
+      // std::cout << line << std::endl;
+      // printList(list.head);
+      eric::mergeSort(&list.head);
+      // insertionSort(&list.head);
+      // printList(list.head);
+
+      // cin>>line;
+      if(chosenLine==-1){
+        double score = eric::findMatchingWord(list.head, positiveWords.data.head,
+                                            negativeWords.data.head);
 
         int convertedScore = int(score);
 
-        eric::compareScore(convertedScore,csvSentimentScore);
-        // cout <<score<<endl;
-        std::cin >> line;
-        // cout<<lineCount<<endl;
-        lineCount++;
+        eric::compareScore(convertedScore, csvSentimentScore);
+        break;
       }
-      skipHeader = false;
+
+      // std::cin >> line;
+      // cout<<lineCount<<endl;
+      lineCount++;
+    }
+    skipHeader = false;
   }
 
-  reviews.recordsToArray(true);
+  // reviews.recordsToArray(true);
   auto stop = high_resolution_clock::now();
-  auto duration = duration_cast<microseconds>(stop-start);
-  cout<<"Time taken: "<<duration.count()<<" microseconds"<<endl;
-  cout<<"End of program"<<endl;
+  auto duration = duration_cast<microseconds>(stop - start);
+  cout << "Time taken: " << duration.count() << " microseconds" << endl;
+  cout << "End of program" << endl;
 }
--- AssignmentThings/ericHeader.h
+++ AssignmentThings/ericHeader.h
@@ -8,9 +8,6 @@ using namespace std;
 
 namespace eric {
 
-
-
-
 // class Words_collection{
 //   public:
 //     int capacity = 20;
@@ -32,7 +29,7 @@ namespace eric {
 //     if(size==capacity){
 //       resize();
 //     }else{
-//       data[size++]=word;      
+//       data[size++]=word;
 //     }
 //   }
 
@@ -40,8 +37,6 @@ namespace eric {
 //     delete[] data;
 //   }
 // };
-
-
 
 // Node class
 class Node {
@@ -61,7 +56,10 @@ class LinkedList {
 public:
   Node *head;
   int size;
-  LinkedList() { this->head = nullptr; size=0; }
+  LinkedList() {
+    this->head = nullptr;
+    size = 0;
+  }
 
   void insertAtBeginning(const std::string &data) {
     Node *newNode = new Node(data);
@@ -118,48 +116,46 @@ public:
 };
 
 // NOTE there are words in both positive and negative that uses things like
-  // "+","-","*" so first thought
-  //  would be do it both the word being checked and the positive/negative but
-  //  there are cases where it's not beneficial such as the positive word "a+"
-  //  as it is not the same meaning as "a".
- std::string removeSpecialCharacter(const std::string &s) {
-    std::string result;
-    for (char c : s) {
-      if (std::isalpha(c) || c == '-' || c == '*' || c == '+') {
-        result += c;
-      }
+// "+","-","*" so first thought
+//  would be do it both the word being checked and the positive/negative but
+//  there are cases where it's not beneficial such as the positive word "a+"
+//  as it is not the same meaning as "a".
+std::string removeSpecialCharacter(const std::string &s) {
+  std::string result;
+  for (char c : s) {
+    if (std::isalpha(c) || c == '-' || c == '*' || c == '+') {
+      result += c;
     }
-    return result;
   }
-  // Array seems to hard to implement for lineSplit cause we do not know the
-  // initial size, and recreating array everytime and copying the elements to
-  // new array is probably bad, so linked list might be best
-  // furthermore, we probably dont need random access anyways since we're
-  // gonna check the words one by one sequentially
+  return result;
+}
+// Array seems to hard to implement for lineSplit cause we do not know the
+// initial size, and recreating array everytime and copying the elements to
+// new array is probably bad, so linked list might be best
+// furthermore, we probably dont need random access anyways since we're
+// gonna check the words one by one sequentially
 eric::LinkedList lineSplit(std::string line, std::string delimiter) {
   // void lineSplit(std::string line, std::string delimiter) {
-    std::stringstream data(line);
-    eric::LinkedList list;
-    std::string word;
-    while (data >> word) {
-      word = removeSpecialCharacter(word);
-      if (!word.empty()) {
-        list.insertAtEnd(word);
-        // std::cout << word << std::endl;
-      }
+  std::stringstream data(line);
+  eric::LinkedList list;
+  std::string word;
+  while (data >> word) {
+    word = removeSpecialCharacter(word);
+    if (!word.empty()) {
+      list.insertAtEnd(word);
+      // std::cout << word << std::endl;
     }
-    return list;
   }
+  return list;
+}
 
-class LinkedListSentimentWords{
- public:
+class LinkedListSentimentWords {
+public:
   LinkedList data;
-  int size = 0 ;
-  LinkedListSentimentWords(){
-   size = 0 ; 
-  }
+  int size = 0;
+  LinkedListSentimentWords() { size = 0; }
 
-  void addWord(const std::string& word){
+  void addWord(const std::string &word) {
     data.insertAtEnd(word);
     size++;
   }
@@ -185,9 +181,7 @@ public:
     }
   }
 
-  std::fstream& getFileStream(){
-    return fileOutput;
-  }
+  std::fstream &getFileStream() { return fileOutput; }
 
   // std::string* recordsToArray(){
   void recordsToArray(bool skipHeader) {
@@ -207,193 +201,230 @@ public:
   }
 };
 
-double calculateSentimentScore( int positiveWordCount, int negativeWordCount){
-  int totalMatchingWordCount= positiveWordCount +negativeWordCount;
-  int rawSentimentScore = positiveWordCount-negativeWordCount;
+double calculateSentimentScore(int positiveWordCount, int negativeWordCount) {
+  int totalMatchingWordCount = positiveWordCount + negativeWordCount;
+  int rawSentimentScore = positiveWordCount - negativeWordCount;
 
-  int minRawScore=-totalMatchingWordCount;
-  int maxRawScore=totalMatchingWordCount;
+  int minRawScore = -totalMatchingWordCount;
+  int maxRawScore = totalMatchingWordCount;
 
-  double normalizedScore = (double)(rawSentimentScore - minRawScore) / (maxRawScore - minRawScore); 
-  double sentimentScore=1+(4*normalizedScore);
+  double normalizedScore =
+      (double)(rawSentimentScore - minRawScore) / (maxRawScore - minRawScore);
+  double sentimentScore = 1 + (4 * normalizedScore);
   return sentimentScore;
 }
 
-void simpleDisplay(Node* positiveWordsFound, Node* negativeWordsFound, int positiveSize, int negativeSize, int sentimentScore){
-  cout<<"Positive Words = "<<positiveSize<<endl;
-  while(positiveWordsFound!=nullptr){
-    cout<<"     "<<positiveWordsFound->data<<endl;
-    positiveWordsFound=positiveWordsFound->next;
-  } 
-  cout<<"\n"<<endl;
-  cout<<"Negative Words = "<<negativeSize<<endl;
-  while(negativeWordsFound!=nullptr){
-    cout<<"     "<<negativeWordsFound->data<<endl;
-    negativeWordsFound=negativeWordsFound->next;
+void simpleDisplay(Node *positiveWordsFound, Node *negativeWordsFound,
+                   int positiveSize, int negativeSize, int sentimentScore) {
+  cout << "Positive Words = " << positiveSize << endl;
+  while (positiveWordsFound != nullptr) {
+    cout << "     " << positiveWordsFound->data << endl;
+    positiveWordsFound = positiveWordsFound->next;
   }
-  cout<<"\n"<<endl;
+  cout << "\n" << endl;
+  cout << "Negative Words = " << negativeSize << endl;
+  while (negativeWordsFound != nullptr) {
+    cout << "     " << negativeWordsFound->data << endl;
+    negativeWordsFound = negativeWordsFound->next;
+  }
+  cout << "\n" << endl;
 
   string grade;
-  if(int(sentimentScore)==4 || int(sentimentScore)==5){
-    grade="Postiive";
-  }else if(int(sentimentScore)==2 || int(sentimentScore)==1){
-    grade="Negative";
-  }else if(int(sentimentScore)==3){
-    grade="Neutral";
-  }else{
-    grade="Error";
+  if (int(sentimentScore) == 4 || int(sentimentScore) == 5) {
+    grade = "Postiive";
+  } else if (int(sentimentScore) == 2 || int(sentimentScore) == 1) {
+    grade = "Negative";
+  } else if (int(sentimentScore) == 3) {
+    grade = "Neutral";
+  } else {
+    grade = "Error";
   }
 
-  cout<<"Sentiment Score (1-5) is "<<sentimentScore<<", thus the rating should be equal to "<<int(sentimentScore)<<" ("<<grade<<").";
+  cout << "Sentiment Score (1-5) is " << sentimentScore
+       << ", thus the rating should be equal to " << int(sentimentScore) << " ("
+       << grade << ").";
 }
 
-int findMatchingWord(Node* reviewLineHead, Node* positiveWordList, Node* negativeWordList){
-    Node *traverse = reviewLineHead; 
-    Node *traversePositive= positiveWordList;
-    Node *traverseNegative = negativeWordList;
-    LinkedList positiveWordsFound;
-    LinkedList negativeWordsFound;
-    int positiveWordCount=0;
-    int negativeWordCount=0;
-    int test;
+struct matchingWordReturn {
+  double *sentimentScore;
+  int *positiveWordCount;
+  int *negativeWordCount;
+  Node *positiveWordsFoundHead;
+  Node *negativeWordsFoundHead;
+};
 
-    while(traverse!=nullptr){
-      Node *traversePositive= positiveWordList;
-      Node *traverseNegative = negativeWordList; 
-      while(traversePositive!=nullptr && traversePositive->data < traverse->data){
-        traversePositive=traversePositive->next;
-      }
-      if(traversePositive!=nullptr && traversePositive->data == traverse->data){
-        // cout<<traversePositive->data<<endl;
-        // cout<<traverse->data<<endl;
-        // cin>>test;
-        positiveWordsFound.insertAtEnd(traverse->data);
-        positiveWordCount++;
-      }
- 
-      while(traverseNegative!=nullptr && traverseNegative->data < traverse->data){
-        traverseNegative=traverseNegative->next;
-      }
-      if(traverseNegative!=nullptr && traverse->data==traverseNegative->data){
-        // cout<<traverseNegative->data<<endl;
-        // cout<<traverse->data<<endl;
-        // cin>>test;
-        negativeWordsFound.insertAtEnd(traverse->data);
-        negativeWordCount++;
-      }
-      traverse=traverse->next;
+int findMatchingWord(Node *reviewLineHead, Node *positiveWordList,
+                     Node *negativeWordList) {
+  Node *traverse = reviewLineHead;
+  Node *traversePositive = positiveWordList;
+  Node *traverseNegative = negativeWordList;
+  LinkedList positiveWordsFound;
+  LinkedList negativeWordsFound;
+  int positiveWordCount = 0;
+  int negativeWordCount = 0;
+  int test;
+
+  while (traverse != nullptr) {
+    Node *traversePositive = positiveWordList;
+    Node *traverseNegative = negativeWordList;
+    while (traversePositive != nullptr &&
+           traversePositive->data < traverse->data) {
+      traversePositive = traversePositive->next;
+    }
+    if (traversePositive != nullptr &&
+        traversePositive->data == traverse->data) {
+      // cout<<traversePositive->data<<endl;
+      // cout<<traverse->data<<endl;
+      // cin>>test;
+      positiveWordsFound.insertAtEnd(traverse->data);
+      positiveWordCount++;
     }
 
-    double sentimentScore = calculateSentimentScore(positiveWordCount, negativeWordCount);
-    // cout<<positiveWordCount<<endl;
-    // cout<<negativeWordCount<<endl;
+    while (traverseNegative != nullptr &&
+           traverseNegative->data < traverse->data) {
+      traverseNegative = traverseNegative->next;
+    }
+    if (traverseNegative != nullptr &&
+        traverse->data == traverseNegative->data) {
+      // cout<<traverseNegative->data<<endl;
+      // cout<<traverse->data<<endl;
+      // cin>>test;
+      negativeWordsFound.insertAtEnd(traverse->data);
+      negativeWordCount++;
+    }
+    traverse = traverse->next;
+  }
 
-    simpleDisplay(positiveWordsFound.head,negativeWordsFound.head,positiveWordCount,negativeWordCount,sentimentScore);
+  double sentimentScore =
+      calculateSentimentScore(positiveWordCount, negativeWordCount);
+  // cout<<positiveWordCount<<endl;
+  // cout<<negativeWordCount<<endl;
 
-    return sentimentScore;
+  matchingWordReturn answer;
+  answer.sentimentScore = &sentimentScore;
+  answer.positiveWordCount = &positiveWordCount;
+  answer.negativeWordCount = &negativeWordCount;
+  answer.positiveWordsFoundHead = positiveWordsFound.head;
+  answer.negativeWordsFoundHead = negativeWordsFound.head;
 
+  simpleDisplay(positiveWordsFound.head, negativeWordsFound.head,
+                positiveWordCount, negativeWordCount, sentimentScore);
+
+  return sentimentScore;
 }
 
-void compareScore(int convertedScore, int csvSentimentScore){
-  cout<<"\n"<<endl;
-  cout<<"Sentiment Score (1-5) = "<<convertedScore<<endl;
-  cout<<"Rating given by user ="<<csvSentimentScore<<endl;
-  cout<<"\n"<<endl;
-  cout<<"Analysis Output:\n"<<endl;
-  if(convertedScore!=csvSentimentScore){
-    cout<<"User's subjective evaluation does not match the sentiment score provided by the analysis. There is an inconsistency between the sentiment score generated by the analysis and the user's evaluation of the sentiment."<<endl;
-  }else{
-    cout<<"User's subjective evaluation matches the sentiment score provided by the analysis. There is a consistency between the sentiment score generated by the analysis and the user's evaluation of the sentiment."<<endl;
+void compareScore(int convertedScore, int csvSentimentScore) {
+  cout << "\n" << endl;
+  cout << "Sentiment Score (1-5) = " << convertedScore << endl;
+  cout << "Rating given by user =" << csvSentimentScore << endl;
+  cout << "\n" << endl;
+  cout << "Analysis Output:\n" << endl;
+  if (convertedScore != csvSentimentScore) {
+    cout << "User's subjective evaluation does not match the sentiment score "
+            "provided by the analysis. There is an inconsistency between the "
+            "sentiment score generated by the analysis and the user's "
+            "evaluation of the sentiment."
+         << endl;
+  } else {
+    cout << "User's subjective evaluation matches the sentiment score provided "
+            "by the analysis. There is a consistency between the sentiment "
+            "score generated by the analysis and the user's evaluation of the "
+            "sentiment."
+         << endl;
   }
 }
 
 // Function to insert a node into the sorted linked list
-void sortedInsert(Node** head_ref, Node* new_node) {
-    Node* current;
-    // Special case for the head end
-    if (*head_ref == nullptr || (*head_ref)->data[0] >= new_node->data[0]) {
-        new_node->next = *head_ref;
-        *head_ref = new_node;
-    } else {
-        // Locate the node before the point of insertion
-        current = *head_ref;
-        while (current->next != nullptr && current->next->data[0] < new_node->data[0]) {
-            current = current->next;
-        }
-        new_node->next = current->next;
-        current->next = new_node;
+void sortedInsert(Node **head_ref, Node *new_node) {
+  Node *current;
+  // Special case for the head end
+  if (*head_ref == nullptr || (*head_ref)->data[0] >= new_node->data[0]) {
+    new_node->next = *head_ref;
+    *head_ref = new_node;
+  } else {
+    // Locate the node before the point of insertion
+    current = *head_ref;
+    while (current->next != nullptr &&
+           current->next->data[0] < new_node->data[0]) {
+      current = current->next;
     }
+    new_node->next = current->next;
+    current->next = new_node;
+  }
 }
 
 // Function to sort a linked list using insertion sort
-void insertionSort(Node** head_ref) {
-    Node* sorted = nullptr;  // Initially, the sorted list is empty
-    Node* current = *head_ref;  // Traverse the given linked list
+void insertionSort(Node **head_ref) {
+  Node *sorted = nullptr;    // Initially, the sorted list is empty
+  Node *current = *head_ref; // Traverse the given linked list
 
-    while (current != nullptr) {
-        Node* next = current->next;  // Store the next node
-        // Insert current in sorted linked list
-        sortedInsert(&sorted, current);
-        // Update current
-        current = next;
-    }
-    // Update the head_ref to point to the sorted list
-    *head_ref = sorted;
+  while (current != nullptr) {
+    Node *next = current->next; // Store the next node
+    // Insert current in sorted linked list
+    sortedInsert(&sorted, current);
+    // Update current
+    current = next;
+  }
+  // Update the head_ref to point to the sorted list
+  *head_ref = sorted;
 }
 
-void printList(Node* node) {
-    while (node != nullptr) {
-        std::cout << node->data << " -> ";
-        node = node->next;
-    }
-    std::cout << "nullptr" << std::endl;
+void printList(Node *node) {
+  while (node != nullptr) {
+    std::cout << node->data << " -> ";
+    node = node->next;
+  }
+  std::cout << "nullptr" << std::endl;
 }
 
-Node* merge(Node* left, Node* right) {
-    if (!left) return right;
-    if (!right) return left;
-    
-    if (left->data[0] <= right->data[0]) {
-        left->next = merge(left->next, right);
-        return left;
-    } else {
-        right->next = merge(left, right->next);
-        return right;
-    }
+Node *merge(Node *left, Node *right) {
+  if (!left)
+    return right;
+  if (!right)
+    return left;
+
+  if (left->data[0] <= right->data[0]) {
+    left->next = merge(left->next, right);
+    return left;
+  } else {
+    right->next = merge(left, right->next);
+    return right;
+  }
 }
 
-void frontBackSplit(Node* source, Node** frontRef, Node** backRef) {
-    Node* fast;
-    Node* slow;
-    if (!source || !source->next) {
-        *frontRef = source;
-        *backRef = nullptr;
-        return;
-    }
-    slow = source;
-    fast = source->next;
-    while (fast) {
-        fast = fast->next;
-        if (fast) {
-            slow = slow->next;
-            fast = fast->next;
-        }
-    }
+void frontBackSplit(Node *source, Node **frontRef, Node **backRef) {
+  Node *fast;
+  Node *slow;
+  if (!source || !source->next) {
     *frontRef = source;
-    *backRef = slow->next;
-    slow->next = nullptr;
+    *backRef = nullptr;
+    return;
+  }
+  slow = source;
+  fast = source->next;
+  while (fast) {
+    fast = fast->next;
+    if (fast) {
+      slow = slow->next;
+      fast = fast->next;
+    }
+  }
+  *frontRef = source;
+  *backRef = slow->next;
+  slow->next = nullptr;
 }
 
-void mergeSort(Node** headRef) {
-    Node* head = *headRef;
-    Node* left;
-    Node* right;
-    if (!head || !head->next) return;
-    
-    frontBackSplit(head, &left, &right);
-    mergeSort(&left);
-    mergeSort(&right);
-    *headRef = merge(left, right);
+void mergeSort(Node **headRef) {
+  Node *head = *headRef;
+  Node *left;
+  Node *right;
+  if (!head || !head->next)
+    return;
+
+  frontBackSplit(head, &left, &right);
+  mergeSort(&left);
+  mergeSort(&right);
+  *headRef = merge(left, right);
 }
 
 } // namespace eric
